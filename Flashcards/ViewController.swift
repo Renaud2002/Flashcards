7af3a690b188582c6700bda629b599edfece8168
Renaud2002 <52769325+Renaud2002@users.noreply.github.com>
2022-09-20 00:27:51 -0400

    optionals 1 and 2 are done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,17 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20.0
+//        card.clipsToBounds = true
+        card.layer.shadowRadius = 20.0
+        card.layer.shadowOpacity = 0.2
+        
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        
     }
 
+    
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        
+        if(frontLabel.isHidden == true){
+            frontLabel.isHidden = false
+        }else{
+            frontLabel.isHidden = true
+        }
     }
+    
     
 }
 
